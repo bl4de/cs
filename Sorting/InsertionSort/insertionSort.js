ab8b4401263b9a40ae8202ff973f6f04618eabca
Rafal Janicki <bloorq@gmail.com>
2021-09-29 22:00:22 +0100

    [Sorting] Fixed insertionSort.js

--- Sorting/InsertionSort/insertionSort.js
+++ Sorting/InsertionSort/insertionSort.js
@@ -1,17 +1,22 @@
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+const numbers = [99, 44, 6, 88, 2, 1, 5, 92, 87];
+const numbers2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function insertionSort(arr) {
     let l = arr.length;
     let tmp;
     let numberOfIterations = 0;
 
-    for (let j = 0; j < l; j++) {
-        let i = 1;
-        while (arr[i] < arr[i - 1]) {
-            tmp = arr[i - 1];
-            arr[i - 1] = arr[i];
-            arr[i] = tmp;
-            i++;
+    for (let j = 0; j <= l; j++) {
+        let i = 0;
+        while (i < l) {
+            if (arr[i] > arr[i + 1]) {
+                tmp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = tmp;
+                i++;
+            } else {
+                i++;
+            }
             numberOfIterations++;
         }
     }
@@ -19,4 +24,6 @@ function insertionSort(arr) {
 }
 
 insertionSort(numbers);
+insertionSort(numbers2);
 console.log(numbers);
+console.log(numbers2);
