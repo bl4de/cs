f4c2f68870ad7888e581701d4dc00f17d8942298
Rafal Janicki <bloorq@gmail.com>
2021-10-04 07:47:40 +0100

    [HackerRank] Capitalize - Python solution

--- HackerRank/capitalize.py
+++ HackerRank/capitalize.py
@@ -9,13 +9,12 @@ def solve(s):
     uppercased = s[0].upper()
     
     # every character precede by spase is also uppercased:
-    for iterator in range(1, len(s) + 1):
-        if s[iterator - 1] == ' ':
+    for iterator in range(1, len(s)):
+        if s[iterator - 1] == ' ' and s[iterator] != ' ':
             uppercased += s[iterator].upper()
         else:
-            uppercased += uppercased[iterator]
+            uppercased += s[iterator]
         iterator += 1
-
     return uppercased 
 
 if __name__ == '__main__':
