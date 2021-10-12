4788a35af162129032b8f8ca759e687b6cbb1d56
Rafal Janicki <bloorq@gmail.com>
2021-10-12 23:48:33 +0100

    [HackerRank] Designer Floor Mat (Python solution - in progress)

--- HackerRank/designer_floor_mat.py
+++ HackerRank/designer_floor_mat.py
@@ -27,15 +27,23 @@
 
 def create_mat(mat_high):
     mat_width = mat_high * 3
+    center = mat_width//2
+    mat = []
+    for iter in range(0, mat_high):
+        line = '-' * (center - iter) + '.' * (iter + 1) + '|' + \
+            '.' * (iter + 1) + '-' * (center - iter)
+        mat.append(line)
+
     welcome = 'WELCOME'
-    print(welcome)
+    # print(welcome)
+    print('\n'.join(mat))
     pass
 
 
 if __name__ == '__main__':
     # mat_high = int(input())
 
-    mat_high = 11
+    mat_high = 7
 
     if mat_high > 0:
         create_mat(mat_high)
