f88a5651bc843c505d76868ba37b272bd92a92d7
Rafal Janicki <bloorq@gmail.com>
2021-10-19 23:14:33 +0100

    [HackerRank] Designer Floor Mat (Python solution - completed)

--- HackerRank/designer_floor_mat.py
+++ HackerRank/designer_floor_mat.py
@@ -24,40 +24,39 @@
 # ------------.|..|..|.------------
 # ---------------.|.---------------
 
+def create_line(line_length, middle_elements):
+    line = ''
+    mel = '.|.'
+
+    line = '-' * ((line_length - middle_elements * len(mel))//2)
+    line += mel * middle_elements
+    line += '-' * ((line_length - middle_elements * len(mel))//2)
+
+    return line
+
 
 def create_mat(mat_high):
     welcome = 'WELCOME'
     mat_width = mat_high * 3
-    center = mat_width//2
     mat = []
-    dots = 1
-    next_char = '.'
+    middle_elements = 1
 
     for iter in range(0, mat_high//2):
-        line = '-' * (center - iter) + \
-            '.' * (iter + 1) + \
-            next_char + \
-            '.' * (iter + 1) + \
-            '-' * (center - iter)
-        dots += 1
-        if dots == 2:
-            next_char = '|'
-            dots = 0
-        else:
-            next_char = '.'
-        mat.append(line)
+        mat.append(create_line(mat_width, middle_elements))
+        middle_elements += 2
 
     line = '-' * ((mat_width - len(welcome)) // 2
-                  + (1 if mat_width % 2 != 0 else 2)) \
+                  + (0 if mat_width % 2 != 0 else 1)) \
         + welcome \
         + '-' * ((mat_width - len(welcome)) // 2 +
-                 (1 if mat_width % 2 != 0 else 2))
+                 (0 if mat_width % 2 != 0 else 1))
 
     mat.append(line)
+    middle_elements -= 2
+
     for iter in range(mat_high//2 - 1, -1, -1):
-        line = '-' * (center - iter) + '.' * (iter + 1) + '|' + \
-            '.' * (iter + 1) + '-' * (center - iter)
-        mat.append(line)
+        mat.append(create_line(mat_width, middle_elements))
+        middle_elements -= 2
 
     print('\n'.join(mat))
 
@@ -65,7 +64,7 @@ def create_mat(mat_high):
 if __name__ == '__main__':
     # mat_high = int(input())
 
-    mat_high = 7
+    mat_high = 11
     if mat_high > 0:
         create_mat(mat_high)
 
