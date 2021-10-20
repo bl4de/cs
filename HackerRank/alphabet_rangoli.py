4e525bc818be08899fd3ed9c37585cfdd35e3997
Rafal Janicki <bloorq@gmail.com>
2021-10-20 21:44:19 +0100

    [HackerRank] Alphabet Rangoli - Python solution (in progress)

--- HackerRank/alphabet_rangoli.py
+++ HackerRank/alphabet_rangoli.py
@@ -1,9 +1,66 @@
+#!/usr/bin/env python3
+# #size 3
+
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
+
+# #size 5
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+# #size 10
+
+# ------------------j------------------
+# ----------------j-i-j----------------
+# --------------j-i-h-i-j--------------
+# ------------j-i-h-g-h-i-j------------
+# ----------j-i-h-g-f-g-h-i-j----------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ----------j-i-h-g-f-g-h-i-j----------
+# ------------j-i-h-g-h-i-j------------
+# --------------j-i-h-i-j--------------
+# ----------------j-i-j----------------
+# ------------------j------------------
+import string
+
+
 def print_rangoli(size):
+    alphabet = string.ascii_lowercase
+    rows = []
+    # create middle row
+    row = ''
+    for iter in range(size - 1, 0, -1):
+        row += alphabet[iter] + '-'
+    for iter in range(0, size):
+        row += alphabet[iter] + ('-' if iter < size - 1 else '')
+
+    rows.append(row)
+
+    print("\n".join(rows))
     pass
 
 
 if __name__ == '__main__':
-    
+
     # size = int(input())
     size = 3
 
