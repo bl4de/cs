3fb89e532c0ba6c1edc5e71682078f4d981706f5
Rafal Janicki <bloorq@gmail.com>
2021-10-21 14:07:42 +0100

    [HackerRank] Alphabet Rangoli - Python solution (in progress)

--- HackerRank/alphabet_rangoli.py
+++ HackerRank/alphabet_rangoli.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python3
-# #size 3
-
-# ----c----
-# --c-b-c--
-# c-b-a-b-c
-# --c-b-c--
-# ----c----
-
-# #size 5
-
-# --------e--------
-# ------e-d-e------
-# ----e-d-c-d-e----
-# --e-d-c-b-c-d-e--
-# e-d-c-b-a-b-c-d-e
-# --e-d-c-b-c-d-e--
-# ----e-d-c-d-e----
-# ------e-d-e------
-# --------e--------
 
 # #size 10
 
@@ -40,20 +21,38 @@
 # --------------j-i-h-i-j--------------
 # ----------------j-i-j----------------
 # ------------------j------------------
+
+# #size 3
+
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
+
+# #size 5
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+
 import string
 
 
 def print_rangoli(size):
     alphabet = string.ascii_lowercase
     rows = []
+    number_of_rows = size * 2 - 1
     # create middle row
-    row = ''
-    for iter in range(size - 1, 0, -1):
-        row += alphabet[iter] + '-'
-    for iter in range(0, size):
-        row += alphabet[iter] + ('-' if iter < size - 1 else '')
-
-    rows.append(row)
+    for i in range(0, number_of_rows // 2):
+        print(i)
 
     print("\n".join(rows))
     pass
@@ -62,6 +61,6 @@ def print_rangoli(size):
 if __name__ == '__main__':
 
     # size = int(input())
-    size = 3
+    size = 5
 
     print_rangoli(size)
