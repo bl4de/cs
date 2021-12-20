084d891c297fc3c62a85e40d6d82d56a65711cb5
Rafal Janicki <bloorq@gmail.com>
2021-12-20 14:29:38 +0000

    [LeetCode] 121.Best Time to Buy and Sell Stock - Python solution O(1) - 153 / 211 test cases passed.

--- LeetCode/121_best_time_to_buy_and_sell_stock.py
+++ LeetCode/121_best_time_to_buy_and_sell_stock.py
@@ -1,13 +1,18 @@
 from typing import List
 
+# def maxProfit(prices: List[int]) -> int:
+#     temp_diff = 0
+#     for day in range(1, len(prices)):
+#         for prev_day in range(0, day):
+#             if (prices[day] - prices[prev_day]) > temp_diff:
+#                 temp_diff = prices[day] - prices[prev_day]
+#     return temp_diff
+
 
 def maxProfit(prices: List[int]) -> int:
-    temp_diff = 0
-    for day in range(1, len(prices)):
-        for prev_day in range(0, day):
-            if (prices[day] - prices[prev_day]) > temp_diff:
-                temp_diff = prices[day] - prices[prev_day]
-    return temp_diff
+    min_price = min(prices)
+    max_price = max(prices[prices.index(min_price):])
+    return max_price - min_price
 
 
 print(maxProfit([7, 1, 5, 3, 6, 4]))
