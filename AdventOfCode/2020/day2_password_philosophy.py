d112e894435c42aa15dfb7039bc22a9d7f3a8cfc
Rafal Janicki <bloorq@gmail.com>
2022-01-06 23:24:59 +0000

    [AdventOfCode] 2020/Day 2/Part 1 - Python solution

--- AdventOfCode/2020/day2_password_philosophy.py
+++ AdventOfCode/2020/day2_password_philosophy.py
@@ -5,8 +5,17 @@ import pathlib
 
 
 def main(data):
-    print(data)
+    correct_passwords = 0
+    criteria = [(p.split(' ')) for p in data.split('\n')]
+    for pwd in criteria:
+        min_occurences, max_occurences = pwd[0].split('-')
+        character = pwd[1][0:1]
+        if int(min_occurences) <= pwd[2].count(character) <= int(max_occurences):
+            correct_passwords += 1
+
+    return correct_passwords
 
 
 data = pathlib.Path("./input.txt").read_text().strip()
-main(data)
+correct_passwords = main(data)
+print(correct_passwords)
