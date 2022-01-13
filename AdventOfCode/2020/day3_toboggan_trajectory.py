ff5116b6b075ec8970c92e32eb932383a374140f
Rafal Janicki <bloorq@gmail.com>
2022-01-13 18:36:39 +0000

    [AdventOfCode] Day 3/Part 1 - in progress

--- AdventOfCode/2020/day3_toboggan_trajectory.py
+++ AdventOfCode/2020/day3_toboggan_trajectory.py
@@ -5,9 +5,21 @@ import pathlib
 
 
 def part_one(data) -> int:
-    result = 0
+    print(data)
+    slop = 0
+    trees = 0
+    iter = 0
+    for lvl in data:
+        print(iter, lvl)
+        trees = trees + 1 if lvl[slop] == '#' else trees
+        print(slop, lvl[slop], trees, '\n\n')
+        slop = slop + 3 if (slop < len(lvl) - 3) else (slop + 3 - len(lvl))
+        iter = iter + 1
+        if iter == 10:
+            break
+
     # process data here...
-    return result
+    return trees
 
 
 def part_two(data) -> int:
@@ -16,7 +28,6 @@ def part_two(data) -> int:
     return result
 
 
-data = pathlib.Path("./tt_input.txt").read_text().strip()
-print(data)
+data = pathlib.Path("./tt_input.txt").read_text().split('\n')[:-1]
 print(part_one(data))  # Part 1. solution
 print(part_two(data))  # Part 2. solution
