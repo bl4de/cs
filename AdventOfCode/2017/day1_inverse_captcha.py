7bd9e7906da8f6d0575014ac0088c8dadfe7872b
Rafal Janicki <bloorq@gmail.com>
2022-01-28 15:03:10 +0000

    [AdventOfCode] 2017 Day 1/Part 1 - Python solution

---
+++ AdventOfCode/2017/day1_inverse_captcha.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# AdventOfCode Challenge Template
+#
+# https://adventofcode.com/events
+#
+
+import pathlib
+
+
+def part_one_solution(data) -> int:
+    '''
+        Solution for Part 1.
+    '''
+    result = 0
+    # process data here...
+    iterator = 1
+    while iterator < len(data):
+        if int(data[iterator - 1]) == int(data[iterator]):
+            result += int(data[iterator])
+        iterator += 1
+    if int(data[len(data) - 1]) == int(data[0]):
+        result += int(data[0])
+    return result
+
+
+def part_two_solution(data) -> int:
+    '''
+        Solution for Part 2.
+    '''
+    result = 0
+    # process data here...
+    return result
+
+
+# challenge input data
+data = pathlib.Path("./input.txt").read_text().strip()
+
+# Part 1. solution
+print(part_one_solution(data))
+
+# Part 2. solution
+print(part_two_solution(data))
