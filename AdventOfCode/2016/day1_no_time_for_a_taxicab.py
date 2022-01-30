2d1339a64e47cdf2845197dde58b26c6eab85e08
Rafal Janicki <bloorq@gmail.com>
2022-01-30 11:02:09 +0000

    [AdventOfCode] 2016 Day 1 (in progress)

---
+++ AdventOfCode/2016/day1_no_time_for_a_taxicab.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# AdventOfCode Challenge Template
+#
+# https://adventofcode.com/events
+#
+
+import pathlib
+
+
+def part_one_solution(data) -> int:
+    '''
+        Solution for Part 1.
+    '''
+    result = 0
+    # process data here...
+    return result
+
+
+def part_two_solution(data) -> int:
+    '''
+        Solution for Part 2.
+    '''
+    result = 0
+    # process data here...
+    return result
+
+
+# challenge input data
+data = pathlib.Path("./input.txt").read_text().strip()
+
+# Part 1. solution
+print(part_one_solution(data))
+
+# Part 2. solution
+print(part_two_solution(data))
