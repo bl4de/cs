2eb266270ef60f4b5e48da0fb8cfb27f168e42b1
Rafal Janicki <bloorq@gmail.com>
2022-02-24 20:42:30 +0000

    [AdventOfCode] 2021 Day 3/Part 1 - Python solution

--- AdventOfCode/2021/day3_binary_diagnostic.py
+++ AdventOfCode/2021/day3_binary_diagnostic.py
@@ -4,7 +4,15 @@
 # https://adventofcode.com/events
 #
 
+from array import array
 import pathlib
+
+
+def binary_array_to_integer(binary_array: array) -> int:
+    '''
+        converts binary array into integer
+    '''
+    return int(''.join(binary_array), 2)
 
 
 def part_one_solution(data) -> int:
@@ -12,14 +20,29 @@ def part_one_solution(data) -> int:
         Solution for Part 1.
     '''
     result = 0
-    gamma_rate = ''
-    epsilon_rate = ''
+    gamma_rate = []
+    epsilon_rate = []
     # process data here...
     data = data.split('\n')
     bits_len = len(data[0])
     for pos in range(0, bits_len):
-        for bits in data.split('\n'):
+        zeros = 0
+        ones = 0
+        for bits in data:
+            if bits[pos] == '1':
+                ones += 1
+            else:
+                zeros += 1
+        print(zeros, ones)
+        if zeros > ones:
+            gamma_rate.append('0')
+            epsilon_rate.append('1')
+        else:
+            gamma_rate.append('1')
+            epsilon_rate.append('0')
 
+    result = binary_array_to_integer(
+        gamma_rate) * binary_array_to_integer(epsilon_rate)
     return result
 
 
