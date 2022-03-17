699908c075ae591a653829ab6e11904b77227f15
Rafal Janicki <bloorq@gmail.com>
2022-03-17 12:35:03 +0000

    [AdventOfCode] 2015 Day 3/Part 1 - Python solution

--- AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
+++ AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
@@ -11,11 +11,23 @@ def part_one_solution(data) -> int:
     '''
         Solution for Part 1.
     '''
-    data = data.split('\n')
-    result = 0
-
-    # process data here...
-
+    result = 1  # first house
+    pos_x = 0  # starting point X
+    pos_y = 0  # starting point Y
+    visited = [(pos_x, pos_y)]
+    for direction in data:
+        if direction == '^':
+            pos_x -= 1
+        if direction == '>':
+            pos_y += 1
+        if direction == '<':
+            pos_y -= 1
+        if direction == 'v':
+            pos_x += 1
+        # process data here...
+        if (pos_x, pos_y) not in visited:
+            visited.append((pos_x, pos_y))
+            result += 1
     return result
 
 
@@ -32,7 +44,7 @@ def part_two_solution(data) -> int:
 
 
 DAY = 3
-DEBUG = True  # False
+DEBUG = False  # False
 
 # read challenge input data
 INPUT_PATH = f"test_input{DAY}.txt" if DEBUG is True else f"input{DAY}.txt"
