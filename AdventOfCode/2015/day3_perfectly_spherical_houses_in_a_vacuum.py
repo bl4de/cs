a20606355330be7861088ee3723995dc75cec483
Rafal Janicki <bloorq@gmail.com>
2022-03-20 09:13:47 +0000

    [AdventOfCode] 2015 Day 3/Part 2 - in progress (wrong result)

--- AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
+++ AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
@@ -37,7 +37,8 @@ def part_two_solution(data) -> int:
     pos_y = 0  # starting point Y
     visited_by_santa = [(pos_x, pos_y)]
     visited_by_robot = [(pos_x, pos_y)]
-    combined = []
+    iterator = 0
+    combined = 0
     for direction in data:
         if direction == '^':
             pos_x -= 1
@@ -47,13 +48,25 @@ def part_two_solution(data) -> int:
             pos_y -= 1
         if direction == 'v':
             pos_x += 1
-    # process data here...
+        if iterator % 2 == 0 and (pos_x, pos_y) not in visited_by_santa:
+            visited_by_santa.append((pos_x, pos_y))
+        else:
+            if (pos_x, pos_y) not in visited_by_robot:
+                visited_by_robot.append((pos_x, pos_y))
+        iterator += 1
 
-    return len(combined)
+    for house in visited_by_santa:
+        if house not in visited_by_robot:
+            combined += 1
+    for house in visited_by_robot:
+        if house not in visited_by_santa:
+            combined += 1
+
+    return combined
 
 
 DAY = 3
-DEBUG = False  # False
+DEBUG = True  # False
 
 # read challenge input data
 INPUT_PATH = f"test_input{DAY}.txt" if DEBUG is True else f"input{DAY}.txt"
