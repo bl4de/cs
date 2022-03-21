43efa5d6397dea8b817bc246e5604a7d2e4712a8
Rafal Janicki <bloorq@gmail.com>
2022-03-21 22:26:20 +0000

    [AdventOfCode] 2015 Day 3/Part 2 - in progress (wrong result)

--- AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
+++ AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
@@ -33,34 +33,45 @@ def part_two_solution(data) -> int:
     '''
         Solution for Part 2.
     '''
-    pos_x = 0  # starting point X
-    pos_y = 0  # starting point Y
-    visited_by_santa = [(pos_x, pos_y)]
-    visited_by_robot = [(pos_x, pos_y)]
+    pos_x_santa = 0  # starting point X
+    pos_x_robot = 0  # starting point X
+    pos_y_santa = 0  # starting point Y
+    pos_y_robot = 0  # starting point Y
+    visited_by_santa = [(pos_x_santa, pos_y_santa)]
+    visited_by_robot = [(pos_x_robot, pos_y_robot)]
     iterator = 0
     combined = 0
     for direction in data:
-        if direction == '^':
-            pos_x -= 1
-        if direction == '>':
-            pos_y += 1
-        if direction == '<':
-            pos_y -= 1
-        if direction == 'v':
-            pos_x += 1
-        if iterator % 2 == 0 and (pos_x, pos_y) not in visited_by_santa:
-            visited_by_santa.append((pos_x, pos_y))
+        if iterator % 2 == 0:
+            if direction == '^':
+                pos_x_santa -= 1
+            if direction == '>':
+                pos_y_santa += 1
+            if direction == '<':
+                pos_y_santa -= 1
+            if direction == 'v':
+                pos_x_santa += 1
+            visited_by_santa.append((pos_x_santa, pos_y_santa))
         else:
-            if (pos_x, pos_y) not in visited_by_robot:
-                visited_by_robot.append((pos_x, pos_y))
+            if direction == '^':
+                pos_x_robot -= 1
+            if direction == '>':
+                pos_y_robot += 1
+            if direction == '<':
+                pos_y_robot -= 1
+            if direction == 'v':
+                pos_x_robot += 1
+            visited_by_robot.append((pos_x_robot, pos_y_robot))
+
         iterator += 1
-        breakpoint()
+        print(visited_by_santa)
+        print(visited_by_robot)
+
     for house in visited_by_santa:
         if house not in visited_by_robot:
             combined += 1
     for house in visited_by_robot:
-        if house not in visited_by_santa:
-            combined += 1
+        combined += 1
 
     return combined
 
