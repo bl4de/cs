2facbab064b2b3350d3a0aa9b2aba188ea0e0f59
Rafal Janicki <bloorq@gmail.com>
2022-03-25 14:35:03 +0000

    [AdventOfCode] Some refactoring

--- AdventOfCode/2015/day2_was_told_there_will_be_no_match.py
+++ AdventOfCode/2015/day2_was_told_there_will_be_no_match.py
@@ -1,46 +1,55 @@
 #!/usr/bin/env python3
-# AdventOfCode Challenge Template
-#
-# https://adventofcode.com/events
-#
+"""
+Advent of Code
+"""
 
 import pathlib
 
 
 def part_one_solution(data) -> int:
-    '''
-        Solution for Part 1.
-    '''
+    """
+    Solution for Part 1.
+    """
     result = 0
     # process data here...
-    for data_item in data.split('\n'):
-        present = data_item.split('x')
+    for data_item in data.split("\n"):
+        present = data_item.split("x")
         box_length = int(present[0])
         box_height = int(present[1])
         box_width = int(present[2])
-        current = (2*box_length*box_height) + (2*box_width*box_height) + (2*box_length *
-                                                                          box_width) + min([box_width*box_height, box_length*box_height, box_length*box_width])
+        current = (
+            (2 * box_length * box_height)
+            + (2 * box_width * box_height)
+            + (2 * box_length * box_width)
+            + min(
+                [
+                    box_width * box_height,
+                    box_length * box_height,
+                    box_length * box_width,
+                ]
+            )
+        )
         result += current
     return result
 
 
 def part_two_solution(data) -> int:
-    '''
-        Solution for Part 2.
-    '''
+    """
+    Solution for Part 2.
+    """
     result = 0
     # process data here...
-    for data_item in data.split('\n'):
+    for data_item in data.split("\n"):
         ribbon = 0
         box_size = 0
-        sizes = [int(s) for s in data_item.split('x')]
+        sizes = [int(s) for s in data_item.split("x")]
 
         maxval = max(sizes)
         print(sizes, maxval)
-        for s in sizes:
-            if s != maxval:
-                print(s, s+s)
-                box_size += s+s
+        for ribbon_size in sizes:
+            if ribbon_size != maxval:
+                print(ribbon_size, ribbon_size + ribbon_size)
+                box_size += ribbon_size + ribbon_size
         ribbon = sizes[0] * sizes[1] * sizes[2]
         print(box_size, ribbon)
         ribbon = ribbon + box_size
@@ -54,11 +63,18 @@ DEBUG = False
 
 # read challenge input data
 if DEBUG:
-    daily_input = pathlib.Path(
-        "/Users/bl4de/playground/cs/AdventOfCode/2015/test_input.txt").read_text('utf_8').strip()
+    daily_input = (
+        pathlib.Path(
+            "/Users/bl4de/playground/cs/AdventOfCode/2015/test_input.txt")
+        .read_text("utf_8")
+        .strip()
+    )
 else:
-    daily_input = pathlib.Path(
-        "/Users/bl4de/playground/cs/AdventOfCode/2015/input.txt").read_text('utf_8').strip()
+    daily_input = (
+        pathlib.Path("/Users/bl4de/playground/cs/AdventOfCode/2015/input.txt")
+        .read_text("utf_8")
+        .strip()
+    )
 
 # Part 1. solution
 print(part_one_solution(daily_input))
--- AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
+++ AdventOfCode/2015/day3_perfectly_spherical_houses_in_a_vacuum.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-'''
+"""
 AdventOfCode Challenge Template
 https://adventofcode.com/events
-'''
+"""
 
 import pathlib
 
 
 def part_one_solution(data) -> int:
-    '''
-        Solution for Part 1.
-    '''
+    """
+    Solution for Part 1.
+    """
     pos_x = 0  # starting point X
     pos_y = 0  # starting point Y
     visited = [(pos_x, pos_y)]
     for direction in data:
-        if direction == '^':
+        if direction == "^":
             pos_x -= 1
-        if direction == '>':
+        if direction == ">":
             pos_y += 1
-        if direction == '<':
+        if direction == "<":
             pos_y -= 1
-        if direction == 'v':
+        if direction == "v":
             pos_x += 1
         # process data here...
         if (pos_x, pos_y) not in visited:
@@ -30,9 +30,9 @@ def part_one_solution(data) -> int:
 
 
 def part_two_solution(data) -> int:
-    '''
-        Solution for Part 2.
-    '''
+    """
+    Solution for Part 2.
+    """
     pos_x_santa = 0  # starting point X
     pos_x_robot = 0  # starting point X
     pos_y_santa = 0  # starting point Y
@@ -43,23 +43,23 @@ def part_two_solution(data) -> int:
     combined = 0
     for direction in data:
         if iterator % 2 == 0:
-            if direction == '^':
+            if direction == "^":
                 pos_x_santa -= 1
-            if direction == '>':
+            if direction == ">":
                 pos_y_santa += 1
-            if direction == '<':
+            if direction == "<":
                 pos_y_santa -= 1
-            if direction == 'v':
+            if direction == "v":
                 pos_x_santa += 1
             visited_by_santa.append((pos_x_santa, pos_y_santa))
         else:
-            if direction == '^':
+            if direction == "^":
                 pos_x_robot -= 1
-            if direction == '>':
+            if direction == ">":
                 pos_y_robot += 1
-            if direction == '<':
+            if direction == "<":
                 pos_y_robot -= 1
-            if direction == 'v':
+            if direction == "v":
                 pos_x_robot += 1
             visited_by_robot.append((pos_x_robot, pos_y_robot))
 
@@ -81,7 +81,7 @@ DEBUG = True  # False
 
 # read challenge input data
 INPUT_PATH = f"test_input{DAY}.txt" if DEBUG is True else f"input{DAY}.txt"
-daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
+daily_input = pathlib.Path(INPUT_PATH).read_text("utf_8").strip()
 
 # Part 1. solution
 print(part_one_solution(daily_input))
