4d7b144e347c86c31a889c207290af408607e115
Rafal Janicki <bloorq@gmail.com>
2022-04-06 10:58:47 +0200

    [AdventOfCode] 2015 Day 5/Part 1 - on progress

---
+++ AdventOfCode/2015/day5_doesnt_he_have_internelves_for_this.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''
+AdventOfCode Challenge Template
+https://adventofcode.com/events
+'''
+
+import pathlib
+
+
+def part_one_solution(data) -> int:
+    '''
+        Solution for Part 1.
+    '''
+    data = data.split('\n')
+    result = 0
+
+    # process data here...
+
+    return result
+
+
+def part_two_solution(data) -> int:
+    '''
+        Solution for Part 2.
+    '''
+    data = data.split('\n')
+    result = 0
+
+    # process data here...
+
+    return result
+
+
+DAY = 5
+DEBUG = True  # False
+
+# read challenge input data
+INPUT_PATH = f"test_input{DAY}.txt" if DEBUG is True else f"input{DAY}.txt"
+daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
+
+
+daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
+
+# Part 1. solution
+print(part_one_solution(daily_input))
+
+# Part 2. solution
+print(part_two_solution(daily_input))
