1eb4f4389a33ddca668a8ef9d267e196c490799b
Rafal Janicki <bloorq@gmail.com>
2022-04-15 11:27:54 +0100

    [AdventOfCode] 2016 Day 1/Part 1 - in progress

--- AdventOfCode/2016/day1_no_time_for_a_taxicab.py
+++ AdventOfCode/2016/day1_no_time_for_a_taxicab.py
@@ -1,35 +1,68 @@
 #!/usr/bin/env python3
-# AdventOfCode Challenge Template
-#
-# https://adventofcode.com/events
-#
+'''
+AdventOfCode Challenge Template
+https://adventofcode.com/events
+'''
 
 import pathlib
+import functools
+import time
 
 
+def timer(func):
+    '''
+        @timer decorator to measure execution time
+        of solution functions
+    '''
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        runtime = time.perf_counter() - start
+        print(f"{func.__name__} took {runtime:.4f} secs")
+        return result
+    return _wrapper
+
+
+@timer
 def part_one_solution(data) -> int:
     '''
         Solution for Part 1.
     '''
+    data = data.split('\n')
     result = 0
+
     # process data here...
+
     return result
 
 
+@timer
 def part_two_solution(data) -> int:
     '''
         Solution for Part 2.
     '''
+    data = data.split('\n')
     result = 0
+
     # process data here...
+
     return result
 
 
-# challenge input data
-data = pathlib.Path("./input.txt").read_text().strip()
+DAY = 1
+DEBUG = True  # UNCOMMENT FOR DEBUG
+# DEBUG = False # UNCOMMENT FOR PRODUCTION
+
+# read challenge input data
+INPUT_PATH = f"test_input{DAY}.txt" if DEBUG is True else f"input{DAY}.txt"
+daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
+
+
+daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
 
 # Part 1. solution
-print(part_one_solution(data))
+print(part_one_solution(daily_input))
 
 # Part 2. solution
-print(part_two_solution(data))
+print(part_two_solution(daily_input))
