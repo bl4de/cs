3736c0de338b97cb232faf88a39896f251de3258
Rafal Janicki <bloorq@gmail.com>
2022-08-22 09:04:27 +0100

    [AdventOfCode] 2015 Day 6/Part 1 - in progress

---
+++ AdventOfCode/2015/day6_probably_a_fire_hazard.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+'''
+AdventOfCode Challenge Template
+
+https://adventofcode.com/events
+
+https://www.reddit.com/r/adventofcode/wiki/solution_megathreads/
+'''
+
+import pathlib
+import functools
+import time
+import sys
+
+
+def timer(func):
+    '''
+    @timer decorator to measure execution time
+    of solution functions
+    '''
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        runtime = time.perf_counter() - start
+        print(f"{func.__name__} took {runtime:.4f} secs")
+        return result
+    return _wrapper
+
+
+@timer
+def part_one_solution(data) -> int:
+    '''
+    Solution for Part 1.
+    '''
+    data = data.split('\n')
+    result = 0
+
+    # process data here...
+
+    return result
+
+
+@timer
+def part_two_solution(data) -> int:
+    '''
+    Solution for Part 2.
+    '''
+    data = data.split('\n')
+    result = 0
+
+    # process data here...
+
+    return result
+
+
+DAY = 6
+DEBUG = True if len(sys.argv) > 1 and sys.argv[1] else False
+
+# read challenge input data
+INPUT_PATH = f"test_input{DAY}.txt" if DEBUG is True else f"input{DAY}.txt"
+daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
+
+
+daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
+
+# Part 1. solution
+print(part_one_solution(daily_input))
+
+# Part 2. solution
+print(part_two_solution(daily_input))
