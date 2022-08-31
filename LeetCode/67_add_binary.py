78f5c88d70662e95cced85050829cde29b05b158
Rafal Janicki <bloorq@gmail.com>
2022-08-31 19:06:09 +0100

    [LeetCode] 67. Add Binary - Python solution

--- LeetCode/67_add_binary.py
+++ LeetCode/67_add_binary.py
@@ -16,15 +16,19 @@ class Solution:
         Converts binary string into int
         '''
         intval = 0
-        for (_, index) in enumerate(s[::-1]):
-            intval += pow(2, int(index))
+        index = len(s)
+        for digit in s:
+            if digit == '1':
+                intval += pow(2, int(index - 1))
+            index -= 1
         return intval
 
     def addBinary(self, a: str, b: str) -> str:
         intval_a = self.calculatemValueFromBinary(a)
         intval_b = self.calculatemValueFromBinary(b)
-        return bin(intval_a + intval_b)
+        return bin(intval_a + intval_b)[2:]
 
 
 solution = Solution()
+print(solution.addBinary("11", "1"))
 print(solution.addBinary("1010", "1011"))
