f89de26e3f35d3f5b6917c24066f2c2cfe1cb07e
Rafal Janicki <bloorq@gmail.com>
2022-09-03 10:39:28 +0100

    [LeetCode] 35. Search Insert Position - in progress

--- LeetCode/35_search_insert_position.py
+++ LeetCode/35_search_insert_position.py
@@ -11,5 +11,32 @@ class Solution:
     LeetCode solution class
     '''
 
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i = 0
+
+        if len(nums) == 1:
+            return 0 if target <= nums[0] else 1
+
+        if target < nums[0]:
+            return 0
+
+        if target > nums[-1]:
+            return len(nums)
+
+        for _index, num in enumerate(nums):
+            if num == target:
+                return _index
+            if num > target and num < nums[_index + 1]:
+                return _index
+            i = _index
+        return i + 1
+
 
 solution = Solution()
+print(solution.searchInsert([1, 3, 5, 6], 5))  # 2
+print(solution.searchInsert([1, 3, 5, 6], 2))  # 1
+print(solution.searchInsert([1, 3, 5, 6], 7))  # 4
+print(solution.searchInsert([1], 0))  # 0
+print(solution.searchInsert([1, 3], 2))  # 1
+print(solution.searchInsert([1, 3], 0))  # 0
+print(solution.searchInsert([1, 3, 5], 2))  # 1
