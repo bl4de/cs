6767c88e41f809b63c919a9b62e26cb46b0d7b47
Rafal Janicki <bloorq@gmail.com>
2022-09-11 12:32:32 +0100

    [LeetCode] 136. Single Number - Python solution

--- LeetCode/136_single_number.py
+++ LeetCode/136_single_number.py
@@ -11,5 +11,22 @@ class Solution:
     LeetCode solution class
     '''
 
+    def singleNumber(self, nums: List[int]) -> int:
+        existings = {
+            "1": [],
+            "2": []
+        }
+        while len(nums) > 0:
+            n = nums.pop()
+            if n in existings["1"]:
+                existings["2"].append(n)
+                existings["1"].remove(n)
+            else:
+                existings["1"].append(n)
+        # should be only one element
+        return existings["1"][0]
 
 solution = Solution()
+print(solution.singleNumber([2, 2, 1]))  # 1
+print(solution.singleNumber([4, 1, 2, 1, 2]))  # 1
+print(solution.singleNumber([1]))  # 1
