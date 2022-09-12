bdaaf5860dafa1ac025c5490138184c339c530c0
Rafal Janicki <bloorq@gmail.com>
2022-09-12 09:23:12 +0100

    [LeetCode] 268. Missing Number - in progress

---
+++ LeetCode/268_missing_number.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, no-self-use,missing-function-docstring, unused-wildcart-import
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+
+    def missingNumber(self, nums: List[int]) -> int:
+        missing_number = 0
+
+        return missing_number
+
+
+solution = Solution()
+print(solution.missingNumber([3, 0, 1]))  # 2
