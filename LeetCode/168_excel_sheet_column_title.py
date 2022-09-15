16942d117520a19b3ecd3f08b55124366093defa
Rafal Janicki <bloorq@gmail.com>
2022-09-15 11:12:36 +0100

    [LeetCode] 168. Excel Sheet Column Title

---
+++ LeetCode/168_excel_sheet_column_title.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, no-self-use,missing-function-docstring, unused-wildcart-import
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+
+    def convertToTitle(self, columnNumber: int) -> str:
+        column_name = ''
+
+        return column_name
+
+
+solution = Solution()
+print(solution.convertToTitle(1))  # 'A'
+print(solution.convertToTitle(28))  # 'AB'
+print(solution.convertToTitle(701))  # 'ZY'
