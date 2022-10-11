adc7f04aa4c1ccb5be0c86f50198910ef26337a3
Rafal Janicki <bloorq@gmail.com>
2022-10-11 08:43:45 +0100

    [LeetCode] 2235. Add Two Integers - Python solution

---
+++ LeetCode/2235_add_two_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, no-self-use,missing-function-docstring, unused-wildcart-import
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+
+
+solution = Solution()
