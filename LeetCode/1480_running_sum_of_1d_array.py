b64062017a776d0ae1504c289a6ad2a36985fdf4
Rafal Janicki <bloorq@gmail.com>
2022-10-13 14:13:32 +0100

    [LeetCode] 1480. Running Sum of 1d Array - in progress

---
+++ LeetCode/1480_running_sum_of_1d_array.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, no-self-use,missing-function-docstring, unused-wildcart-import
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = []
+        return running_sum
+
+
+solution = Solution()
+print(solution.runningSum([1, 2, 3, 4]))  # [1,3,6,10]
