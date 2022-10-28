09894542938a6a14dd68dc69b51394c2f1950673
Rafal Janicki <bloorq@gmail.com>
2022-10-28 09:48:52 +0100

    [LeetCode] 2047. Number of Valid Words in a Sentence - in progress

--- LeetCode/2047_number_of_valid_words_in_a_sentence.py
+++ LeetCode/2047_number_of_valid_words_in_a_sentence.py
@@ -4,16 +4,26 @@
     LeetCode solution class
 '''
 from typing import List, Optional
-
+import re
+import string
 
 class Solution:
     '''
     LeetCode solution class
     '''
 
+    def check_if_word_is_valid(self, word: str) -> bool:
+        valid_characters = string.ascii_lowercase + "0123456789" + "!,. "
+
+        return True
+
+
     def countValidWords(self, sentence: str) -> int:
         valid_words = 0
-
+        words = sentence.split(' ')
+        for word in words:
+            if self.check_if_word_is_valid(word):
+                valid_words += 1
         return valid_words
 
 
