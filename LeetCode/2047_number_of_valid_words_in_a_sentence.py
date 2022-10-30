818b6f7f8558a57baf2e3e03d14f4ce34f9046e9
Rafal Janicki <bloorq@gmail.com>
2022-10-30 20:04:26 +0000

    [LeetCode] 2047. Number of Valid Words in a Sentence - in progress

--- LeetCode/2047_number_of_valid_words_in_a_sentence.py
+++ LeetCode/2047_number_of_valid_words_in_a_sentence.py
@@ -19,9 +19,12 @@ class Solution:
         lowercase characters ("a-b" is valid, but "-ab" and "ab-" are not valid).
         '''
         hyphen_index = word.rfind('-')
-        print(hyphen_index)
-        if hyphen_index == -1 or hyphen_index == 1 or hyphen_index == len(word - 1):
-            return False
+        if hyphen_index == -1:
+            return True  # no hyphen in word
+        if hyphen_index == 1 or hyphen_index == len(word - 1):
+            return False  # hyphen is at the beginning or end of the word
+
+        # return True only when 'a-b' sequence occurs:
         return word[hyphen_index - 1] == 'a' and word[hyphen_index + 1] == 'b'
 
     def check_if_word_is_valid(self, word: str) -> bool:
@@ -31,12 +34,13 @@ class Solution:
         valid_words = 0
         words = sentence.split(' ')
         for word in words:
-            if self.check_if_word_is_valid(word) and self.check_for_hypen(word):
-                valid_words += 1
+            if len(word) > 1:
+                if self.check_if_word_is_valid(word) and self.check_for_hypen(word):
+                    valid_words += 1
         return valid_words
 
 
 solution = Solution()
 print(solution.countValidWords("cat and  dog"))  # 3
-print(solution.countValidWords("!this  1-s b8d!"))  # 0
-print(solution.countValidWords("alice and  bob are playing stone-game10"))  # 5
+# print(solution.countValidWords("!this  1-s b8d!"))  # 0
+# print(solution.countValidWords("alice and  bob are playing stone-game10"))  # 5
