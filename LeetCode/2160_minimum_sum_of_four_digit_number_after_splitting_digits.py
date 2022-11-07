825a3c21b8a6a116e2b4d3c236cb28b2e4355cd6
Rafal Janicki <bloorq@gmail.com>
2022-11-07 10:47:23 +0000

    2160. Minimum Sum of Four Digit Number After Splitting Digits - in progress

---
+++ LeetCode/2160_minimum_sum_of_four_digit_number_after_splitting_digits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+
+    def minimumSum(self, num: int) -> int:
+        minimum_sum = 0
+
+        return minimum_sum
+
+
+solution = Solution()
+print(solution.minimumSum(2932))  # 52
+print(solution.minimumSum(4009))  # 13
