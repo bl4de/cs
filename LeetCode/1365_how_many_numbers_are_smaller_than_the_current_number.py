3bdd94ec0ccd9c25c058ce9c200c9ce1c0f4e0c2
Rafal Janicki <bloorq@gmail.com>
2022-11-10 11:16:22 +0000

    [LeetCode] 1365. How Many Numbers Are Smaller Than the Current Number - in progress

---
+++ LeetCode/1365_how_many_numbers_are_smaller_than_the_current_number.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+
+
+solution = Solution()
