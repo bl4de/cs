9cc977f5b6f36498200b031375dc106f4dc7e609
Rafal Janicki <bloorq@gmail.com>
2022-11-11 12:44:39 +0000

    [LeetCode] template.py update

--- LeetCode/template.py
+++ LeetCode/template.py
@@ -11,5 +11,33 @@ class Solution:
     LeetCode solution class
     '''
 
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def debug(self, provided, expected) -> bool:
+        '''
+        Compares provided result to expected
+        Returns true if equal
+        '''
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
 
 solution = Solution()
+
+solution.debug(1, 1)
+solution.debug(1, 2)
