e7d05dedc5b525f8e082bec5e8dde8105ef37645
Rafal Janicki <bloorq@gmail.com>
2022-12-07 00:57:36 +0000

    [AdventOfCode] 2022 Day 4 Part 1 solution - in progress

---
+++ AdventOfCode/2022/day4_camp_cleanup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, no-self-use
+'''
+AdventOfCode Challenge Template
+
+https://adventofcode.com/events
+
+https://www.reddit.com/r/adventofcode/wiki/solution_megathreads/
+'''
+
+import pathlib
+import functools
+import time
+import sys
+from typing import List
+
+
+def timer(func):
+    '''
+    @timer decorator to measure execution time
+    of solution functions
+    '''
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        runtime = time.perf_counter() - start
+        print(f"{func.__name__} took {runtime:.4f} secs")
+        return result
+    return _wrapper
+
+
+@timer
+def part_one_solution(data) -> int:
+    '''
+    Solution for Part 1.
+    '''
+    data = data.split('\n')
+    result = 0
+
+    for d in data:
+        pairs = d.split(',')
+        range1 = "".join([str(c) for c in range(
+            int(pairs[0].split('-')[0]), int(pairs[0].split('-')[1]) + 1)])
+        range2 = "".join([str(c) for c in range(
+            int(pairs[1].split('-')[0]), int(pairs[1].split('-')[1]) + 1)])
+
+        if int(range1) < int(range2) or int(range2) < int(range1):
+            result += 1
+    return result
+
+
+@timer
+def part_two_solution(data) -> int:
+    '''
+    Solution for Part 2.
+    '''
+    data = data.split('\n')
+    result = 0
+
+    # process data here...
+
+    return result
+
+
+DAY = 4
+DEBUG = True if len(sys.argv) > 1 and sys.argv[1] else False
+
+# read challenge input data
+INPUT_PATH = f"test_input{DAY}.txt" if DEBUG is True else f"input{DAY}.txt"
+daily_input = pathlib.Path(INPUT_PATH).read_text('utf_8').strip()
+
+# Part 1. solution
+print(part_one_solution(daily_input))
+
+# Part 2. solution
+print(part_two_solution(daily_input))
