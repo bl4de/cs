368e5c54d40394533e04678128dcaac3a2479eb0
Rafal Janicki <bloorq@gmail.com>
2022-12-25 13:09:41 +0000

    2469. Convert the Temperature - in progress

---
+++ LeetCode/2469_convert_the_temperature.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        '''
+        Compares provided result to expected
+        Returns true if equal
+        '''
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def convertTemperature(self, celsius: float) -> List[float]:
+        '''
+        Solution function goes here
+        '''
+        return []
+
+
+solution = Solution()
+solution.test(solution.convertTemperature(36.50), [309.65000, 97.70000])
+solution.test(solution.convertTemperature(122.11), [395.26000, 251.79800])
