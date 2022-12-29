63645961d15159dc4e8c862a301b12926afdd994
Rafal Janicki <bloorq@gmail.com>
2022-12-29 19:44:30 +0000

    [LeetCode] 1952. Three Divisors - in progress

---
+++ LeetCode/1952_three_divisors.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        '''
+        Compares provided result to expected
+        Returns true if equal
+        '''
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def isThree(self, n: int) -> bool:
+        '''
+        Solution function goes here
+        '''
+        has_three = True
+
+        return has_three
+
+
+solution = Solution()
+solution.test(solution.isThree(2), False)
+solution.test(solution.isThree(4), True)
