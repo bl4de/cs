e52bbbcebaa50eda148f1e9fa9f0851e8cff9045
Rafal Janicki <bloorq@gmail.com>
2023-01-01 11:19:14 +0000

    [LeetCode] 1108. Defanging an IP Address - Python solution

---
+++ LeetCode/1108_defanging_an_ip_address.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        '''
+        Compares provided result to expected
+        Returns true if equal
+        '''
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def defangIPaddr(self, address: str) -> str:
+        '''
+        Solution function goes here
+        '''
+        return address.replace('.', '[.]')
+
+
+solution = Solution()
+solution.test(solution.defangIPaddr("1.1.1.1"), "1[.]1[.]1[.]1")
+solution.test(solution.defangIPaddr("255.100.50.0"), "255[.]100[.]50[.]0")
