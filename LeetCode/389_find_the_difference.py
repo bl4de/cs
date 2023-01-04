e188551451ad4e610ee3838f95cdde6af05bf69b
Rafal Janicki <bloorq@gmail.com>
2023-01-04 10:15:55 +0000

    [LeetCode] 389. Find the Difference - Python solution

---
+++ LeetCode/389_find_the_difference.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        '''
+        Compares provided result to expected
+        Returns true if equal
+        '''
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def findTheDifference(self, s: str, t: str) -> str:
+        '''
+        Solution function goes here
+        '''
+        for c in t:
+            if t.count(c) > s.count(c):
+                return c
+        return ''
+
+
+solution = Solution()
+solution.test(solution.findTheDifference("abcd", "abcde"), "e")
+solution.test(solution.findTheDifference("", "y"), "y")
