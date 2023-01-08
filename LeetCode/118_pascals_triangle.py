f64c5519351caf456e50d334c56f169e488ce24d
Rafal Janicki <bloorq@gmail.com>
2023-01-08 16:43:22 +0000

    [LeetCode] 118. Pascal's Triangle - in progress

---
+++ LeetCode/118_pascals_triangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+'''
+    LeetCode solution class
+'''
+from typing import List, Optional
+
+
+class Solution:
+    '''
+    LeetCode solution class
+    '''
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        '''
+        Compares provided result to expected
+        Returns true if equal
+        '''
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        '''
+        Solution function goes here
+        '''
+        triangle = [[1]]
+
+        return triangle
+
+
+solution = Solution()
+solution.test(solution.generate(5), [[1], [1, 1], [
+              1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]])
+solution.test(solution.generate(1), [[1]])
