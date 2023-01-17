cfb37c6853169a56f8fcf20bd0ed1d42e5d5bc68
Rafal Janicki <bloorq@gmail.com>
2023-01-17 07:56:41 +0000

    [LeetCode] 2016. Maximum Difference Between Increasing Elements - in progress

---
+++ LeetCode/2016_maximum_difference_between_increasing_elements.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+"""
+    LeetCode solution class
+"""
+from typing import List, Optional
+import string
+
+
+class Solution:
+    """
+    LeetCode solution class
+    """
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        """
+        Compares provided result to expected
+        Returns true if equal
+        """
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def maximumDifference(self, nums: List[int]) -> int:
+        """
+        Solution function goes here
+        """
+        max_diff = 0
+
+        return max_diff
+
+
+solution = Solution()
+solution.test(solution.maximumDifference([7, 1, 5, 4]), 4)
+solution.test(solution.maximumDifference([9, 4, 3, 2]), -1)
+solution.test(solution.maximumDifference([1, 5, 2, 10]), 9)
