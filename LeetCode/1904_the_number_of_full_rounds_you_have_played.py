91f0e54cb4fa5e037a3c3fae575bf1e3a62d7fc2
Rafal Janicki <bloorq@gmail.com>
2023-01-27 10:01:27 +0000

    [LeetCode] 1904. The Number of Full Rounds You Have Played - in progress

---
+++ LeetCode/1904_the_number_of_full_rounds_you_have_played.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+"""
+    LeetCode solution class
+"""
+from typing import List, Optional
+import string
+
+
+class Solution:
+    """
+    LeetCode solution class
+    """
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        """
+        Compares provided result to expected
+        Returns true if equal
+        """
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def numberOfRounds(self, loginTime: str, logoutTime: str) -> int:
+        """
+        Solution function goes here
+        """
+        rounds = 0
+
+        return rounds
+
+
+solution = Solution()
+solution.test(solution.numberOfRounds("09:31", "10:14"), 1)
+solution.test(solution.numberOfRounds("21:30", "03:00"), 22)
