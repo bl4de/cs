2e48a823e14215190a383652b4fc03ffd24d6fb8
Rafal Janicki <bloorq@gmail.com>
2023-01-28 11:47:54 +0000

    [LeetCode] 1816. Truncate Sentence - Python solution

---
+++ LeetCode/1816_truncate_sentence.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+"""
+    LeetCode solution class
+"""
+from typing import List, Optional
+import string
+
+class Solution:
+    """
+    LeetCode solution class
+    """
+    colors = {
+        "black": '\33[30m',
+        "red": '\33[31m',
+        "green": '\33[32m',
+        "yellow": '\33[33m',
+        "blue": '\33[34m',
+        "magenta": '\33[35m',
+        "cyan": '\33[36m',
+        "white": '\33[37m',
+        "grey": '\33[90m',
+        "lightblue": '\33[94m'
+    }
+    endline = '\33[0m'
+    PASS = f"{colors['green']}[+] PASS {endline}"
+    FAIL = f"{colors['red']}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        """
+        Compares provided result to expected
+        Returns true if equal
+        """
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def truncateSentence(self, s: str, k: int) -> str:
+        """
+        Solution function goes here
+        """
+        return " ".join(s.split(" ")[0:k])
+
+solution = Solution()
+solution.test(solution.truncateSentence("Hello how are you Contestant", 4), "Hello how are you")
+solution.test(solution.truncateSentence("What is the solution to this problem", 4), "What is the solution")
+solution.test(solution.truncateSentence("chopper is not a tanuki", 5), "chopper is not a tanuki")
