f5daff1b187beaef27908f2a6fa48bde2f47789c
Rafal Janicki <bloorq@gmail.com>
2023-02-01 10:30:52 +0000

    [LeetCode] 1684. Count the Number of Consistent Strings - in progress

---
+++ LeetCode/1684_count_the_number_of_consistent_strings.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+"""
+    LeetCode solution class
+    https://leetcode.com/problems/count-the-number-of-consistent-strings/
+"""
+from typing import List, Optional
+import string
+
+class Solution:
+    """
+    LeetCode solution class
+    """
+    endline = '\33[0m'
+    green = '\33[32m'
+    red = '\33[31m'
+    PASS = f"{green}[+] PASS {endline}"
+    FAIL = f"{red}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        """
+        Compares provided result to expected
+        """
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
+
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        """
+        Solution function goes here
+        """
+        result = 0
+        return result
+
+solution = Solution()
+solution.test(solution.countConsistentStrings("ab", ["ad","bd","aaab","baa","badab"]), 2)
+solution.test(solution.countConsistentStrings("abc", ["a","b","c","ab","ac","bc","abc"]), 7)
+solution.test(solution.countConsistentStrings("cad", ["cc","acd","b","ba","bac","bad","ac","d"]), 4)
