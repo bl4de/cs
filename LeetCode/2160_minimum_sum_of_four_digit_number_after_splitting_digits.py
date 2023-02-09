f83bab11a976395b632da9c593a5e3e94abf5fa7
Rafal Janicki <bloorq@gmail.com>
2023-02-09 09:59:29 +0000

    [LeetCode] 2160. Minimum Sum of Four Digit Number After Splitting Digits - in progress

--- LeetCode/2160_minimum_sum_of_four_digit_number_after_splitting_digits.py
+++ LeetCode/2160_minimum_sum_of_four_digit_number_after_splitting_digits.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python3
 # pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
-'''
+"""
     LeetCode solution class
-'''
+"""
 from typing import List, Optional
-
+import string
 
 class Solution:
-    '''
+    """
     LeetCode solution class
-    '''
+    """
+    endline = '\33[0m'
+    green = '\33[32m'
+    red = '\33[31m'
+    PASS = f"{green}[+] PASS {endline}"
+    FAIL = f"{red}[+] FAIL {endline}"
+
+    def test(self, provided, expected) -> bool:
+        """
+        Compares provided result to expected
+        """
+        print(f"\n output  : {provided}\n expected: {expected}")
+        print(f" {self.PASS if provided == expected else self.FAIL}")
+        return provided == expected
 
     def minimumSum(self, num: int) -> int:
-        minimum_sum = 0
-
-        return minimum_sum
-
+        """
+        Solution function goes here
+        """
+        result = 0
+        digits = [int(digit) for digit in list(str(num))]
+        print(digits)
+        return result
 
 solution = Solution()
-print(solution.minimumSum(2932))  # 52
-print(solution.minimumSum(4009))  # 13
+solution.test(solution.minimumSum(2932), 52)
+solution.test(solution.minimumSum(4009), 13)
+
