a32426cbe2aaa131d24cada07575e97683b97813
Rafal Janicki <bloorq@gmail.com>
2023-02-09 10:20:58 +0000

    [LeetCode] 2160. Minimum Sum of Four Digit Number After Splitting Digits - Python solution

--- LeetCode/2160_minimum_sum_of_four_digit_number_after_splitting_digits.py
+++ LeetCode/2160_minimum_sum_of_four_digit_number_after_splitting_digits.py
@@ -2,9 +2,11 @@
 # pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
 """
     LeetCode solution class
+    https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
 """
 from typing import List, Optional
 import string
+import itertools
 
 class Solution:
     """
@@ -24,13 +26,27 @@ class Solution:
         print(f" {self.PASS if provided == expected else self.FAIL}")
         return provided == expected
 
+    def smallest_sum_of_pair(self, digits:List[int]) -> int:
+        possible_pairs = [
+            (digits[0], int(f"{digits[1]}{digits[2]}{digits[3]}")),
+            (int(f"{digits[0]}{digits[1]}"),int(f"{digits[2]}{digits[3]}")),
+            (int(f"{digits[0]}{digits[1]}{digits[2]}"), digits[3])
+        ]
+        smallest_sum = 1008 # 9 + 999 -> maximum sum
+        for pair in possible_pairs:
+            current_sum = pair[0] + pair[1]
+            smallest_sum = current_sum if current_sum < smallest_sum else smallest_sum
+        return smallest_sum
+
     def minimumSum(self, num: int) -> int:
         """
         Solution function goes here
         """
-        result = 0
+        result = 1008
         digits = [int(digit) for digit in list(str(num))]
-        print(digits)
+        for subset in itertools.permutations(digits):
+            current_sum = self.smallest_sum_of_pair(list(subset))
+            result = current_sum if current_sum < result else result
         return result
 
 solution = Solution()
