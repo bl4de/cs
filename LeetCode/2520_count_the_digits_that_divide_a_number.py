7c8db0ae1eaff1effd4b1220088e4ced2ea38abc
Rafal Janicki <bloorq@gmail.com>
2023-02-19 15:47:14 +0000

    [LeetCode] 2520. Count the Digits That Divide a Number - in progress

---
+++ LeetCode/2520_count_the_digits_that_divide_a_number.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+"""
+    LeetCode solution class
+    https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+"""
+from typing import List, Optional
+import string
+import sys
+from abstract_solution import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    def countDigits(self, num: int) -> int:
+        """
+        Solution function goes here
+        """
+        result = 0
+        return result
+
+
+sys.setrecursionlimit(1000)
+solution = Solution()
+solution.test(solution.countDigits(7), 1)
+solution.test(solution.countDigits(121), 2)
+solution.test(solution.countDigits(1248), 4)
