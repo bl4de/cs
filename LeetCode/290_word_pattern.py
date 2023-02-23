9249aaac44fa68507e9e9f2fee3193631988eecf
Rafal Janicki <bloorq@gmail.com>
2023-02-23 12:16:34 +0000

    [LeetCode] 290. Word Pattern - in progress

--- LeetCode/290_word_pattern.py
+++ LeetCode/290_word_pattern.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
 """
     LeetCode solution class
+    https://leetcode.com/problems/word-pattern/
 """
 from typing import List, Optional
 import string
@@ -10,13 +11,17 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def wordPattern(self, pattern: str, s: str) -> bool:
         """
         Solution function goes here
         """
-        result = 0
-        return result
+
+        return True
 
 
 sys.setrecursionlimit(1000)
 solution = Solution()
+solution.test(solution.wordPattern(pattern="abba", s="dog cat cat dog"), True)
+solution.test(solution.wordPattern(
+    pattern="abba", s="dog cat cat fish"), False)
+solution.test(solution.wordPattern(pattern="aaaa", s="dog cat cat dog"), False)
