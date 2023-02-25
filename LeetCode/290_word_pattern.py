177e8e1592aea26b8be23f1b34d1fe47b692ffbf
Rafal Janicki <bloorq@gmail.com>
2023-02-25 11:30:34 +0000

    [LeetCode] 290. Word Pattern - Python solution

--- LeetCode/290_word_pattern.py
+++ LeetCode/290_word_pattern.py
@@ -11,11 +11,26 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
+
     def wordPattern(self, pattern: str, s: str) -> bool:
         """
         Solution function goes here
         """
+        dictionary = {}
 
+        characters = list(pattern)
+        words = s.split()
+
+        if len(characters) != len(words):
+            return False
+
+        for index, character in enumerate(characters):
+            if words[index] not in dictionary.keys() and character not in dictionary.values():
+                dictionary[words[index]] = character
+            if words[index] in dictionary.keys() and dictionary[words[index]] != character:
+                return False
+            if words[index] not in dictionary.keys() and character in dictionary.values():
+                return False
         return True
 
 
