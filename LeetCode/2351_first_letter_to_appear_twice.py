efb7364e63d9765cd0636dc98e016db7f6ec5421
Rafal Janicki <bloorq@gmail.com>
2023-03-01 11:36:42 +0000

    [LeetCode] 2351. First Letter to Appear Twice - in progress

---
+++ LeetCode/2351_first_letter_to_appear_twice.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
+"""
+    LeetCode solution class
+    https://leetcode.com/problems/first-letter-to-appear-twice/description/
+"""
+from typing import List, Optional
+import string
+import sys
+from abstract_solution import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    def repeatedCharacter(self, s: str) -> str:
+        """
+        Solution function goes here
+        """
+        result = 0
+        return result
+
+
+sys.setrecursionlimit(1000)
+solution = Solution()
+solution.test(solution.repeatedCharacter("abccbaacz"), "c")
+solution.test(solution.repeatedCharacter("abcdd"), "d")
