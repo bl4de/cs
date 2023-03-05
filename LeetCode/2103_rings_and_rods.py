3ba978ecde969601802f48f8b5e8f071b587dbf8
Rafal Janicki <bloorq@gmail.com>
2023-03-05 10:29:18 +0000

    [LeetCode] 2103. Rings and Rods - Python solution

--- LeetCode/2103_rings_and_rods.py
+++ LeetCode/2103_rings_and_rods.py
@@ -16,10 +16,29 @@ class Solution(AbstractSolution):
         Solution function goes here
         """
         result = 0
+        rods = {}
+        for rod in range(0, 10):
+            if rings.count(str(rod)) < 3:
+                continue
+            rods[str(rod)] = []
+
+        rings = list(rings)
+        rings.reverse()
+
+        for iterator in range(0, len(rings), 2):
+            if rings[iterator] in rods.keys():
+                rods[rings[iterator]].append(rings[iterator + 1])
+
+        for rod, rings in rods.items():
+            if len(set(rings)) == 3:
+                result += 1
         return result
 
 
 sys.setrecursionlimit(1000)
 solution = Solution()
-solution.test(solution.countPoints("B0B6G0R6R0R6G9"), 1)
+solution.test(solution.countPoints(
+    rings="B0B6G0R6R0R6G9"), 1)
+solution.test(solution.countPoints(
+    rings="B0R0G0R9R0B0G0"), 1)
 solution.test(solution.countPoints("G4"), 0)
