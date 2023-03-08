f2844599a00200ac59a1e7d0731b5d4f918e31a8
Rafal Janicki <bloorq@gmail.com>
2023-03-08 10:21:32 +0000

    {leetCode] 1662. Check If Two String Arrays are Equivalent - in progress

---
+++ LeetCode/1662_check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
+"""
+    LeetCode solution class
+    https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+"""
+from typing import List, Optional
+import string
+import sys
+from abstract_solution import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        """
+        Solution function goes here
+        """
+        result = False
+        return result
+
+
+sys.setrecursionlimit(1000)
+solution = Solution()
+
+solution.test(solution.arrayStringsAreEqual(
+    word1=["ab", "c"], word2=["a", "bc"]), True)
+solution.test(solution.arrayStringsAreEqual(
+    word1=["a", "cb"], word2=["ab", "c"]), False)
+solution.test(solution.arrayStringsAreEqual(
+    ["abc", "d", "defg"], word2=["abcddefg"]), True)
+
+
+# solution.timer_start()
+# for i in range(0, 1000000):
+#     pass
+# solution.timer_stop()
