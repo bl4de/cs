9ef614ab82b97a436f54a160e73055ba53b4134a
Rafal Janicki <bloorq@gmail.com>
2023-03-10 13:37:24 +0000

    [LeetCode] 2129_capitalize_the_title.py - in progress

---
+++ LeetCode/2129_capitalize_the_title.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
+"""
+    LeetCode solution class
+    https://leetcode.com/problems/capitalize-the-title/description/
+"""
+from typing import List, Optional
+import string
+import sys
+from abstract_solution import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    def capitalizeTitle(self, title: str) -> str:
+        """
+        Solution function goes here
+        """
+        result = 0
+        return result
+
+
+sys.setrecursionlimit(1000)
+solution = Solution()
+
+solution.test(solution.capitalizeTitle(
+    title="capiTalIze tHe titLe"), "Capitalize The Title")
+solution.test(solution.capitalizeTitle(
+    title="First leTTeR of EACH Word"), "First Letter of Each Word")
+solution.test(solution.capitalizeTitle(
+    title="i lOve leetcode"), "i Love Leetcode")
+
+# solution.timer_start()
+# for i in range(0, 1000000):
+#     pass
+# solution.timer_stop()
