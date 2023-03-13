f10a7e5d2768b8725ad3f8ca8dbad5446ca59321
Rafal Janicki <bloorq@gmail.com>
2023-03-13 10:59:21 +0000

    [LeetCode] 521. Longest Uncommon Subsequence I - in progress

--- LeetCode/521_longest_uncommon_subsequence_i.py
+++ LeetCode/521_longest_uncommon_subsequence_i.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
 """
 from typing import List, Optional
 import string
@@ -10,7 +11,7 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def findLUSlength(self, a: str, b: str) -> int:
         """
         Solution function goes here
         """
@@ -21,6 +22,9 @@ class Solution(AbstractSolution):
 sys.setrecursionlimit(1000)
 solution = Solution()
 
+solution.test(solution.findLUSlength(a="aba", b="cdc"), 3)
+solution.test(solution.findLUSlength(a="aaa", b="bbb"), 3)
+solution.test(solution.findLUSlength(a="aaa", b="aaa"), -1)
 
 # solution.timer_start()
 # for i in range(0, 1000000):
