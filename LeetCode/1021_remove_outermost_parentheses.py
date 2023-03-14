006c68d5ed50999f273a3283c052f2e320bb3c35
Rafal Janicki <bloorq@gmail.com>
2023-03-14 02:42:33 +0000

    [LeetCode] 1021. Remove Outermost Parentheses - initial commit

---
+++ LeetCode/1021_remove_outermost_parentheses.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
+"""
+    LeetCode solution class
+"""
+from typing import List, Optional
+import string
+import sys
+from abstract_solution import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    def solutionFunction(self):
+        """
+        Solution function goes here
+        """
+        result = 0
+        return result
+
+
+sys.setrecursionlimit(1000)
+solution = Solution()
+
+
+# solution.timer_start()
+# for i in range(0, 1000000):
+#     pass
+# solution.timer_stop()
