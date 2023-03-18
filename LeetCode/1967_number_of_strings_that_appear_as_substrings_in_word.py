6e0b8fbdbe78387e319d9f6c16a9880810c151c4
Rafal Janicki <bloorq@gmail.com>
2023-03-18 23:02:44 +0000

    [LeetCode] 1967. Number of Strings That Appear as Substrings in Word - Python solution

--- LeetCode/1967_number_of_strings_that_appear_as_substrings_in_word.py
+++ LeetCode/1967_number_of_strings_that_appear_as_substrings_in_word.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
 """
 from typing import List, Optional
 import string
@@ -10,17 +11,25 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
         """
         Solution function goes here
         """
         result = 0
+        for pattern in patterns:
+            if pattern in word:
+                result += 1
         return result
 
 
 sys.setrecursionlimit(1000)
 solution = Solution()
 
+solution.test(solution.numOfStrings(
+    patterns=["a", "abc", "bc", "d"], word="abc"), 3)
+solution.test(solution.numOfStrings(
+    patterns=["a", "b", "c"], word="aaaaabbbbb"), 2)
+solution.test(solution.numOfStrings(patterns=["a", "a", "a"], word="ab"), 3)
 
 # solution.timer_start()
 # for i in range(0, 1000000):
