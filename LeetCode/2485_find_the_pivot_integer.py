80ad9c4980257a39dd044c36477eb52900e9a657
Rafal Janicki <bloorq@gmail.com>
2023-03-19 12:16:32 +0000

    [LeetCode] 2485. Find the Pivot Integer - in progress

--- LeetCode/2485_find_the_pivot_integer.py
+++ LeetCode/2485_find_the_pivot_integer.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/find-the-pivot-integer/
 """
 from typing import List, Optional
 import string
@@ -10,17 +11,21 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def pivotInteger(self, n: int) -> int:
         """
         Solution function goes here
         """
         result = 0
+
         return result
 
 
 sys.setrecursionlimit(1000)
 solution = Solution()
 
+solution.test(solution.pivotInteger(8), 6)
+solution.test(solution.pivotInteger(1), 1)
+solution.test(solution.pivotInteger(4), -1)
 
 # solution.timer_start()
 # for i in range(0, 1000000):
