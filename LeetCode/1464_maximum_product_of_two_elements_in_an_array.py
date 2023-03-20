408ade29fbede299c8975e4da2a3df4b612992c2
Rafal Janicki <bloorq@gmail.com>
2023-03-20 11:48:00 +0000

    [LeetCode] 1464. Maximum Product of Two Elements in an Array - Python solution

--- LeetCode/1464_maximum_product_of_two_elements_in_an_array.py
+++ LeetCode/1464_maximum_product_of_two_elements_in_an_array.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
 """
 from typing import List, Optional
 import string
@@ -10,17 +11,19 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def maxProduct(self, nums: List[int]) -> int:
         """
         Solution function goes here
         """
-        result = 0
-        return result
+        return (nums.pop(nums.index(max(nums))) - 1) * (nums.pop(nums.index(max(nums))) - 1)
 
 
 sys.setrecursionlimit(1000)
 solution = Solution()
 
+solution.test(solution.maxProduct(nums=[3, 4, 5, 2]), 12)
+solution.test(solution.maxProduct(nums=[1, 5, 4, 5]), 16)
+solution.test(solution.maxProduct(nums=[3, 7]), 12)
 
 # solution.timer_start()
 # for i in range(0, 1000000):
