bef3c20606572bc5bdb3ef3e048ba7b482014cef
Rafal Janicki <bloorq@gmail.com>
2023-03-23 09:17:21 +0000

    [LeetCode] 1342. Number of Steps to Reduce a Number to Zero - in progress

--- LeetCode/1342_number_of_steps_to_reduce_a_number_to_zero.py
+++ LeetCode/1342_number_of_steps_to_reduce_a_number_to_zero.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 """
 from typing import List, Optional
 import string
@@ -10,7 +11,7 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def numberOfSteps(self, num: int) -> int:
         """
         Solution function goes here
         """
@@ -21,6 +22,9 @@ class Solution(AbstractSolution):
 sys.setrecursionlimit(1000)
 solution = Solution()
 
+solution.test(solution.numberOfSteps(num=14), 6)
+solution.test(solution.numberOfSteps(num=8), 4)
+solution.test(solution.numberOfSteps(num=123), 12)
 
 # solution.timer_start()
 # for i in range(0, 1000000):
