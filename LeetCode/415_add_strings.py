b2b1dcef9526171b975e64b814e78e850ad7b39b
Rafal Janicki <bloorq@gmail.com>
2023-03-28 10:03:35 +0100

    [LeetCode] 415. Add Strings - Python solution

--- LeetCode/415_add_strings.py
+++ LeetCode/415_add_strings.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/add-strings/description/
 """
 from typing import List, Optional
 import string
@@ -10,17 +11,19 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def addStrings(self, num1: str, num2: str) -> str:
         """
         Solution function goes here
         """
-        result = 0
-        return result
+        return str(int(num1) + int(num2))
 
 
 sys.setrecursionlimit(1000)
 solution = Solution()
 
+solution.test(solution.addStrings(num1="11", num2="123"), "134")
+solution.test(solution.addStrings(num1="456", num2="77"), "533")
+solution.test(solution.addStrings(num1="0", num2="0"), "0")
 
 # solution.timer_start()
 # for i in range(0, 1000000):
