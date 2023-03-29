e6c78b5f654ad31d7c10cb9901d768dca6f5d3ce
Rafal Janicki <bloorq@gmail.com>
2023-03-29 10:03:13 +0100

    [LeetCode] 1470. Shuffle the Array - in progress

--- LeetCode/1470_shuffle_the_array.py
+++ LeetCode/1470_shuffle_the_array.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/shuffle-the-array/
 """
 from typing import List, Optional
 import string
@@ -10,17 +11,20 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    def solutionFunction(self):
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
         """
         Solution function goes here
         """
-        result = 0
+        result = []
         return result
 
 
 sys.setrecursionlimit(1000)
 solution = Solution()
 
+solution.test(solution.shuffle(nums = [2,5,1,3,4,7], n = 3),  [2,3,5,4,1,7] )
+solution.test(solution.shuffle(nums = [1,2,3,4,4,3,2,1], n = 4), [1,4,2,3,3,2,4,1])
+solution.test(solution.shuffle(nums = [1,1,2,2], n = 2),  [1,2,1,2])
 
 # solution.timer_start()
 # for i in range(0, 1000000):
