43ac93bd7ef060a24ff601c3b656865c8714cc67
Rafal Janicki <bloorq@gmail.com>
2023-03-31 11:11:01 +0100

    [LeetCode] 2006. Count Number of Pairs With Absolute Difference K - initial commit

--- LeetCode/2006_count_number_of_pairs_with_absolute_difference_k.py
+++ LeetCode/2006_count_number_of_pairs_with_absolute_difference_k.py
@@ -1,44 +1,41 @@
 #!/usr/bin/env python3
-# pylint: disable=invalid-name, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate
+# pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
-    https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/
 """
 from typing import List, Optional
 import string
+import sys
+import cProfile
+from abstract_solution import AbstractSolution
 
 
-class Solution:
-    """
-    LeetCode solution class
-    """
-    endline = '\33[0m'
-    green = '\33[32m'
-    red = '\33[31m'
-    PASS = f"{green}[+] PASS {endline}"
-    FAIL = f"{red}[+] FAIL {endline}"
-
-    def test(self, provided, expected) -> bool:
+class Solution(AbstractSolution):
+    
+    def solve(self):
         """
-        Compares provided result to expected
+        Solution runner called from profiler
         """
-        print(f"\n output  : {provided}\n expected: {expected}")
-        print(f" {self.PASS if provided == expected else self.FAIL}")
-        return provided == expected
-
-    def countKDifference(self, nums: List[int], k: int) -> int:
+        self.test(self.solution(), None)
+        
+        
+    def solution(self):
         """
         Solution function goes here
         """
         result = 0
-        for n in nums:
-            if n + k in nums:
-                result += 1
-            if k - n in nums:
-                result += 1
         return result
 
+
+sys.setrecursionlimit(1000)
+profiler = cProfile.Profile()
 solution = Solution()
-solution.test(solution.countKDifference([1, 2, 2, 1], 1), 4)
-solution.test(solution.countKDifference([1, 3], 3), 0)
-solution.test(solution.countKDifference([3, 2, 1, 5, 4], 2), 3)
+
+solution.timer_start()
+profiler.enable()
+for i in range(0, 1):
+    profiler.run("solution.solve()")
+solution.timer_stop()
+
+profiler.print_stats()
+profiler.disable()
