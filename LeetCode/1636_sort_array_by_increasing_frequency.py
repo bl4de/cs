2ac9a1e04a9cb8a71236ebe61bb6a955b0511151
Rafal Janicki <bloorq@gmail.com>
2023-04-04 12:29:53 +0100

    [LeetCode] 1636. Sort Array by Increasing Frequency - initial commit

---
+++ LeetCode/1636_sort_array_by_increasing_frequency.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
+"""
+    LeetCode solution class
+"""
+from typing import List, Optional
+import string
+import sys
+import cProfile
+from abstract_solution import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    
+    def solve(self):
+        """
+        Solution runner called from profiler
+        """
+        self.test(self.solution(), None)
+
+    def solution(self):
+        """
+        Solution function goes here
+        """
+        result = 0
+        return result
+
+
+sys.setrecursionlimit(1000)
+profiler = cProfile.Profile()
+solution = Solution()
+
+solution.timer_start()
+profiler.enable()
+for i in range(0, 1):
+    profiler.run("solution.solve()")
+solution.timer_stop()
+
+profiler.print_stats()
+profiler.disable()
