48bd9ce0b1fb1b8a6674322ca199607d2c565737
Rafal Janicki <bloorq@gmail.com>
2023-04-04 12:31:51 +0100

    [LeetCode] 1636. Sort Array by Increasing Frequency - in progress

--- LeetCode/1636_sort_array_by_increasing_frequency.py
+++ LeetCode/1636_sort_array_by_increasing_frequency.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/sort-array-by-increasing-frequency/description/
 """
 from typing import List, Optional
 import string
@@ -16,9 +17,12 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(nums = [1,1,2,2,2,3]), [3,1,1,2,2,2])
+        self.test(self.solution(nums = [2,3,1,3,2]), [1,3,3,2,2])
+        self.test(self.solution(nums = [-1,1,-6,4,5,-6,1,4,1]), [5,-1,4,4,-6,-6,1,1,1])
+        
 
-    def solution(self):
+    def solution(self, nums: List[int]) -> List[int]:
         """
         Solution function goes here
         """
