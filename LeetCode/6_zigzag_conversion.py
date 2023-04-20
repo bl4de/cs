d05dded207c9e5fb79793891feb5d7491e91dff3
Rafal Janicki <bloorq@gmail.com>
2023-04-20 13:13:26 +0100

    [LeetCode] 6. Zigzag Conversion - in progress

--- LeetCode/6_zigzag_conversion.py
+++ LeetCode/6_zigzag_conversion.py
@@ -12,23 +12,25 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    
+
     def solve(self):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(s = "PAYPALISHIRING", numRows = 3), "PAHNAPLSIIGYIR")
+        self.test(self.solution(s="PAYPALISHIRING",
+                  numRows=3), "PAHNAPLSIIGYIR")
         # P   A   H   N
         # A P L S I I G
         # Y   I   R
-        
-        self.test(self.solution(s = "PAYPALISHIRING", numRows = 4), "PINALSIGYAHRPI")
+
+        self.test(self.solution(s="PAYPALISHIRING",
+                  numRows=4), "PINALSIGYAHRPI")
         # P     I    N
         # A   L S  I G
         # Y A   H R
         # P     I
-        
-        self.test(self.solution(s = "A", numRows = 1), "A")
+
+        self.test(self.solution(s="A", numRows=1), "A")
         # A
 
     def solution(self, s: str, numRows: int) -> str:
@@ -36,9 +38,32 @@ class Solution(AbstractSolution):
         Solution function goes here
         """
         result = ""
-        
-        
+        rows = [None] * numRows
+        for row in range(len(rows)):
+            rows[row] = [None] * (len(s) // numRows)
+            
+        print(rows)
+        c = 0
+        row = 0
+        col = 0
+        direction = 'down'
+        for c in s:
+            if direction == 'up':
+                col = col + 1
+            
+            rows[row][col] = c
+            print(rows)
+            if row == (numRows - 1):
+                row = row - 1
+                direction = 'up'
+            if row > 0:
+                row = row + 1
+                direction = 'down'
+            
+
+        print(rows)
         return result
+
 
 NUMS_OF_EXECUTION = 1
 SHOW_PROFILER_STATS = True
