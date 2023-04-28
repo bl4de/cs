abb7b46d4efc1216292cc6232079d6ddd6eeb487
Rafal Janicki <bloorq@gmail.com>
2023-04-28 13:40:23 +0100

    [LeetCode] 4. Median of Two Sorted Arrays - in progress

--- LeetCode/4_median_of_two_sorted_arrays.py
+++ LeetCode/4_median_of_two_sorted_arrays.py
@@ -26,6 +26,9 @@ class Solution(AbstractSolution):
         """
         merged = []
         iterations = len(nums2) if len(nums2) < len(nums1) else len(nums1)
+        
+        # TODO:
+        # merge two sorted arrays:
         for i in range(iterations):
             if nums1[i] > nums2[i]:
                 merged.append(nums2[i])
@@ -33,6 +36,9 @@ class Solution(AbstractSolution):
             else:
                 merged.append(nums1[i])
                 merged.append(nums2[i])
+        
+        
+        
         if len(nums1) > len(nums2):
             merged = merged + nums1[iterations:]
         if len(nums2) > len(nums1):
