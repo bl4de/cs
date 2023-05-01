684d7547a1fa4c90339ac1b590e08e0e01e8f28e
Rafal Janicki <bloorq@gmail.com>
2023-05-01 11:52:26 +0100

    [LeetCode] 8. String to Integer (atoi) - in progress

--- LeetCode/8_string_to_integer_atoi.py
+++ LeetCode/8_string_to_integer_atoi.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/string-to-integer-atoi/
 """
 from typing import List, Optional
 import string
@@ -16,9 +17,12 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(s = "42"), 42)
+        self.test(self.solution(s = "   -42"), 42)
+        self.test(self.solution(s = "4193 with words"), 4193)
+        
 
-    def solution(self):
+    def solution(self, s: str) -> int:
         """
         Solution function goes here
         """
