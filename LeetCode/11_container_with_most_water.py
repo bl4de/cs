419f884dd19e9214c8ba5366cb68c43b39884657
Rafal Janicki <bloorq@gmail.com>
2023-05-10 12:00:08 +0100

    [LeetCode] 11. Container With Most Water - solution 1 (Time Limit Exceeded)

--- LeetCode/11_container_with_most_water.py
+++ LeetCode/11_container_with_most_water.py
@@ -12,20 +12,25 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    
+
     def solve(self):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(height = [1,8,6,2,5,4,8,3,7]), 49)
-        self.test(self.solution(height = [1,1]), 1)
+        self.test(self.solution(height=[1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
+        self.test(self.solution(height=[1, 1]), 1)
 
     def solution(self, height: List[int]) -> int:
         """
         Solution function goes here
         """
-        result = 0
-        return result
+        sums = {}
+        for i in range(0, len(height)):
+            for j in range(i, len(height)):
+                sums[(height[i], height[j], j-i)] = height[i] * \
+                    (j - i) if height[i] < height[j] else height[j] * (j - i)
+        return max(sums.values())
+
 
 NUMS_OF_EXECUTION = 1
 SHOW_PROFILER_STATS = True
