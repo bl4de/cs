a23a923777d9b509d52a42dd54eb3bac22dcf89c
Rafal Janicki <bloorq@gmail.com>
2023-05-14 09:19:17 +0100

    [LeetCode] 12. Integer to Roman - in progress

--- LeetCode/12_integer_to_roman.py
+++ LeetCode/12_integer_to_roman.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/integer-to-roman/
 """
 from typing import List, Optional
 import string
@@ -11,19 +12,22 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    
+
     def solve(self):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(num=3), "III")
+        self.test(self.solution(num=58), "LVIII")
+        self.test(self.solution(num=1994), "MCMXCIV")
 
-    def solution(self):
+    def solution(self, num: int) -> str:
         """
         Solution function goes here
         """
-        result = 0
+        result = ""
         return result
+
 
 NUMS_OF_EXECUTION = 1
 SHOW_PROFILER_STATS = True
