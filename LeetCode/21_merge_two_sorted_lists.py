15a96c5e44eebfee99d8766d37a9a586f01f7c46
Rafal Janicki <bloorq@gmail.com>
2023-05-23 12:06:20 +0100

    [LeetCode] 21. Merge Two Sorted Lists - in progress

--- LeetCode/21_merge_two_sorted_lists.py
+++ LeetCode/21_merge_two_sorted_lists.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/merge-two-sorted-lists/
 """
 from typing import List, Optional
 import string
@@ -10,20 +11,30 @@ import cProfile
 from abstract_solution import AbstractSolution
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(AbstractSolution):
-    
+
     def solve(self):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(list1=[1, 2, 4],
+                  list2=[1, 3, 4]), [1, 1, 2, 3, 4, 4])
+        self.test(self.solution(list1=[], list2=[]), [])
+        self.test(self.solution(list1=[], list2=[0]), [0])
 
-    def solution(self):
+    def solution(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         """
         Solution function goes here
         """
         result = 0
         return result
+
 
 NUMS_OF_EXECUTION = 1
 SHOW_PROFILER_STATS = True
