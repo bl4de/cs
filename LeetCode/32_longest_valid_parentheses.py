dd1f46ac22f9f36a83d60396727fabfacb4bb366
Rafal Janicki <bloorq@gmail.com>
2023-05-24 11:44:07 +0100

    [LeetCode] 32. Longest Valid Parentheses - initial commit

---
+++ LeetCode/32_longest_valid_parentheses.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
+"""
+    LeetCode solution class
+"""
+from typing import List, Optional
+import string
+import sys
+import cProfile
+from abstract_solution import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    
+    def solve(self):
+        """
+        Solution runner called from profiler
+        """
+        self.test(self.solution(), None)
+
+    def solution(self):
+        """
+        Solution function goes here
+        """
+        result = 0
+        return result
+
+NUMS_OF_EXECUTION = 1
+SHOW_PROFILER_STATS = True
+
+sys.setrecursionlimit(1000)
+profiler = cProfile.Profile()
+solution = Solution()
+
+solution.timer_start()
+profiler.enable()
+for i in range(0, NUMS_OF_EXECUTION):
+    profiler.run("solution.solve()")
+solution.timer_stop()
+
+if SHOW_PROFILER_STATS:
+    profiler.print_stats(sort='calls')
+profiler.disable()
