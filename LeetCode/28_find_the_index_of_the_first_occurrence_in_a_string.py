d163b28d03808512dccd4de6714aae78ae8edcef
Rafal Janicki <bloorq@gmail.com>
2023-06-05 10:40:06 +0100

    [LeetCode] 28. Find the Index of the First Occurrence in a String - in progress

--- LeetCode/28_find_the_index_of_the_first_occurrence_in_a_string.py
+++ LeetCode/28_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 """
 from typing import List, Optional
 import string
@@ -16,9 +17,10 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(haystack = "sadbutsad", needle = "sad"), 0)
+        self.test(self.solution(haystack = "leetcode", needle = "leeto"), -1)
 
-    def solution(self):
+    def solution(self, haystack: str, needle: str) -> int:
         """
         Solution function goes here
         """
