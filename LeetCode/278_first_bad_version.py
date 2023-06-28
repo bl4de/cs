16cf8356890d4c2a02bc4e3662714ba3f4eec7d4
Rafal Janicki <bloorq@gmail.com>
2023-06-28 11:01:37 +0100

    [LeetCode] 278. First Bad Version - in progress

--- LeetCode/278_first_bad_version.py
+++ LeetCode/278_first_bad_version.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/first-bad-version/
 """
 from typing import List, Optional
 import string
@@ -10,20 +11,26 @@ import cProfile
 from abstract_solution import AbstractSolution
 
 
+def isBadVersion(version: int) -> bool:
+    return True
+
+
 class Solution(AbstractSolution):
-    
+
     def solve(self):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(n=5, bad=4), 4)
+        self.test(self.solution(n=1, bad=1), 1)
 
-    def solution(self):
+    def solution(self, n: int) -> int:
         """
         Solution function goes here
         """
         result = 0
         return result
+
 
 NUMS_OF_EXECUTION = 1
 SHOW_PROFILER_STATS = True
