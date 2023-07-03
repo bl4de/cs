4a3386d0b4304737ca104fc04029beb2a9c89729
Rafal Janicki <bloorq@gmail.com>
2023-07-03 21:58:08 +0100

    [LeetCode] 367. Valid Perfect Square - in progress

--- LeetCode/367_valid_perfect_square.py
+++ LeetCode/367_valid_perfect_square.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/valid-perfect-square/
 """
 from typing import List, Optional
 import string
@@ -16,13 +17,14 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(num = 16), True)
+        self.test(self.solution(num = 14), False)
 
-    def solution(self):
+    def solution(self, num: int) -> bool:
         """
         Solution function goes here
         """
-        result = 0
+        result = False
         return result
 
 NUMS_OF_EXECUTION = 1
