17cd59d36e0585f475ea2fd618e47d7bb254865c
Rafal Janicki <bloorq@gmail.com>
2023-07-04 11:55:08 +0100

    [LeetCode] 367. Valid Perfect Square - solution 1 (Time Limit Exceeded)

--- LeetCode/367_valid_perfect_square.py
+++ LeetCode/367_valid_perfect_square.py
@@ -19,13 +19,19 @@ class Solution(AbstractSolution):
         """
         self.test(self.solution(num = 16), True)
         self.test(self.solution(num = 14), False)
+        self.test(self.solution(num = 1), True)
+        self.test(self.solution(num = 9), True)
 
     def solution(self, num: int) -> bool:
         """
         Solution function goes here
         """
-        result = False
-        return result
+        if num == 1:
+            return True
+        for i in range(1, num//4 + 2):
+            if i*i == num:
+                return True
+        return False
 
 NUMS_OF_EXECUTION = 1
 SHOW_PROFILER_STATS = True
