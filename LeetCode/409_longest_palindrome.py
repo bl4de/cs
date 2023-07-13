8a37071d40676261288ae88761bd79ddfe03693b
Rafal Janicki <bloorq@gmail.com>
2023-07-13 11:49:18 +0100

    [LeetCode] 409. Longest Palindrome - in progress

--- LeetCode/409_longest_palindrome.py
+++ LeetCode/409_longest_palindrome.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/longest-palindrome/
 """
 from typing import List, Optional
 import string
@@ -11,19 +12,21 @@ from abstract_solution import AbstractSolution
 
 
 class Solution(AbstractSolution):
-    
+
     def solve(self):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(s="abccccdd"), 7)
+        self.test(self.solution(s="a"), 1)
 
-    def solution(self):
+    def solution(self, s: str) -> int:
         """
         Solution function goes here
         """
         result = 0
         return result
+
 
 NUMS_OF_EXECUTION = 1
 SHOW_PROFILER_STATS = True
