661de98e1b1aa93a3bc423beb902184e65c62028
Rafal Janicki <bloorq@gmail.com>
2023-08-08 09:32:01 +0200

    [LeetCode] 434. Number of Segments in a String - in progress

--- LeetCode/434_number_of_segments_in_a_string.py
+++ LeetCode/434_number_of_segments_in_a_string.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/number-of-segments-in-a-string/
 """
 from typing import List, Optional
 import string
@@ -20,9 +21,10 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(s="Hello, my name is John"), 5)
+        self.test(self.solution(s="Hello"), 1)
 
-    def solution(self):
+    def solution(self, s: str) -> int:
         """
         Solution function goes here
         """
