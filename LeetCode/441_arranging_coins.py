f837001d5fcbeb07a3f694be5d5010d724d72520
Rafal Janicki <bloorq@gmail.com>
2023-08-14 08:24:11 +0100

    [LeetCode] 441. Arranging Coins - in progress

--- LeetCode/441_arranging_coins.py
+++ LeetCode/441_arranging_coins.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/arranging-coins/
 """
 from typing import List, Optional
 import string
@@ -20,9 +21,10 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(5), 2)
+        self.test(self.solution(8), 3)
 
-    def solution(self):
+    def solution(self, n: int) -> int:
         """
         Solution function goes here
         """
