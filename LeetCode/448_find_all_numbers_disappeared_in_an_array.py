a672cb4496ff709ba8a369a8095537f5b747fe3a
Rafal Janicki <bloorq@gmail.com>
2023-08-24 08:35:18 +0100

    [LeetCode] 448. Find All Numbers Disappeared in an Array - in progress

--- LeetCode/448_find_all_numbers_disappeared_in_an_array.py
+++ LeetCode/448_find_all_numbers_disappeared_in_an_array.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 """
 from typing import List, Optional
 import string
@@ -20,13 +21,14 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(nums=[4, 3, 2, 7, 8, 2, 3, 1]), [5, 6])
+        self.test(self.solution(nums=[1, 1]), [2])
 
-    def solution(self):
+    def solution(self, nums: List[int]) -> List[int]:
         """
         Solution function goes here
         """
-        result = 0
+        result = []
         return result
 
 
