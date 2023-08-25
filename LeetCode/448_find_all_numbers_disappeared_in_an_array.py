b4df7dc5ed37b6f8f03fc1a84ae6fbf1a2f0c589
Rafal Janicki <bloorq@gmail.com>
2023-08-25 11:41:12 +0100

    [LeetCode] 448. Find All Numbers Disappeared in an Array - solution 1 - Time Limit Exceeded

--- LeetCode/448_find_all_numbers_disappeared_in_an_array.py
+++ LeetCode/448_find_all_numbers_disappeared_in_an_array.py
@@ -29,6 +29,9 @@ class Solution(AbstractSolution):
         Solution function goes here
         """
         result = []
+        for i in range(1, len(nums) + 1):
+            if i not in nums:
+                result.append(i)
         return result
 
 
