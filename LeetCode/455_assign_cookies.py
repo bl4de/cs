fb746e00f8dda4012e381ddcf5ec74f64ab417d8
Rafal Janicki <bloorq@gmail.com>
2023-08-27 06:29:27 +0100

    [LeetCode] 455. Assign Cookies - in progress

--- LeetCode/455_assign_cookies.py
+++ LeetCode/455_assign_cookies.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name, missing-class-docstring, import-error, too-few-public-methods, unused-import, no-self-use,missing-function-docstring,consider-using-enumerate,consider-iterating-dictionary
 """
     LeetCode solution class
+    https://leetcode.com/problems/assign-cookies/
 """
 from typing import List, Optional
 import string
@@ -20,9 +21,10 @@ class Solution(AbstractSolution):
         """
         Solution runner called from profiler
         """
-        self.test(self.solution(), None)
+        self.test(self.solution(g=[1, 2, 3], s=[1, 1]), 1)
+        self.test(self.solution(g=[1, 2], s=[1, 2, 3]), 2)
 
-    def solution(self):
+    def solution(self, g: List[int], s: List[int]) -> int:
         """
         Solution function goes here
         """
