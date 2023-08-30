cea3db42cbab1c0f14ae5b6fd0bce694936f3927
Rafal Janicki <bloorq@gmail.com>
2023-08-30 05:42:14 +0100

    [LeetCode] 500. Keyboard Row - Python solution ~64% perf.

--- LeetCode/500_keyboard_row.py
+++ LeetCode/500_keyboard_row.py
@@ -30,12 +30,41 @@ class Solution(AbstractSolution):
         """
         Solution function goes here
         """
-        keyboard = [
-            "qwertyuiop",
-            "asdfghjkl",
-            "zxcvbnm"
-        ]
         result = []
+        keyboard = {
+            "q": 0,
+            "w": 0,
+            "e": 0,
+            "r": 0,
+            "t": 0,
+            "y": 0,
+            "u": 0,
+            "i": 0,
+            "o": 0,
+            "p": 0,
+
+            "a": 1,
+            "s": 1,
+            "d": 1,
+            "f": 1,
+            "g": 1,
+            "h": 1,
+            "j": 1,
+            "k": 1,
+            "l": 1,
+
+            "z": 2,
+            "x": 2,
+            "c": 2,
+            "v": 2,
+            "b": 2,
+            "n": 2,
+            "m": 2
+        }
+
+        for word in words:
+            if len(set([keyboard[c] for c in word.lower()])) == 1:
+                result.append(word)
         return result
 
 
