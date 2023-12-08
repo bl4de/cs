0eb177945757a5198d12a562010b1b0a34d5bf43
Rafal Janicki <bloorq@gmail.com>
2023-12-08 00:19:59 +0000

    [AdventOfCode] 2023 Day 2 Part 1 solution

--- AdventOfCode/2023/day2_cube_conundrum.py
+++ AdventOfCode/2023/day2_cube_conundrum.py
@@ -14,6 +14,10 @@ import time
 import sys
 from typing import List
 
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
 
 def timer(func):
     '''
@@ -39,13 +43,34 @@ def extract_cubes(game: str) -> dict:
     green = 0
     for g in game.split(';'):
         for color in g.split(','):
-            print(color)
-
+            color_details = color.strip().split(' ')
+            if color_details[1] == 'blue' and int(color_details[0]) > blue:
+                blue = int(color_details[0])
+            if color_details[1] == 'red' and int(color_details[0]) > red:
+                red = int(color_details[0])
+            if color_details[1] == 'green' and int(color_details[0]) > green:
+                green = int(color_details[0])
     return {
         'blue': blue,
         'red': red,
         'green': green
     }
+
+
+def extract_game_number(game: str) -> int:
+    '''
+    returns integer representing Game number
+    '''
+    return int(game.split(' ')[1])
+
+
+def check_game(game) -> bool:
+    '''
+    checks if game is possible
+    '''
+    if game['red'] > MAX_RED or game['blue'] > MAX_BLUE or game['green'] > MAX_GREEN:
+        return False
+    return True
 
 
 @timer
@@ -59,7 +84,9 @@ def part_one_solution(data) -> int:
     # process data here...
     for game in data:
         game = game.split(':')
-        extract_cubes(game=game[1])
+        cubes = extract_cubes(game=game[1])
+        if check_game(cubes):
+            result += extract_game_number(game=game[0])
     return result
 
 
