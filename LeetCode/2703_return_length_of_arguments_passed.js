c3da292d32c106e5aa4a012033eda55fade65aa5
Rafal Janicki <bloorq@gmail.com>
2024-03-10 15:34:38 +0000

    [LeetCode] 2703. Return Length of Arguments Passed - JavaScript solution

--- LeetCode/2703_return_length_of_arguments_passed.js
+++ LeetCode/2703_return_length_of_arguments_passed.js
@@ -1,4 +1,11 @@
 /** LeetCode JavaScript template solution */
+// https://leetcode.com/problems/return-length-of-arguments-passed/description/
 
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
 
-console.log("Ok.");
